6640f5bd9b2bdf869340850a2caa6f443aa43464
princesscorn <34575300+princesscorn@users.noreply.github.com>
2021-01-24 11:05:31 -0600

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component.jsx';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.';
+import {auth} from './firebase/firebase.utils';
+
+import './App.css';
+
+
 
 /*
 const HatsPage = () => (
@@ -14,18 +19,45 @@ const HatsPage = () => (
 );
 */
 
-function App() {
-  return (
-    <div>
-      {/* hello world! */}
-      {/* <HomePage /> */}
-      <Header />
-      <Switch>
-        <Route exact path = '/' component = {HomePage} />
-        <Route exact path = '/shop' component = {ShopPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged (user => {
+      this.setState({currentUser: user});
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render()
+  {
+    return (
+      <div>
+        {/* hello world! */}
+        {/* <HomePage /> */}
+        <Header currentUser = {this.state.currentUser} />
+        <Switch>
+          <Route exact path = '/' component = {HomePage} />
+          <Route exact path = '/shop' component = {ShopPage} />
+          <Route exact path = '/signin' component = {SignInAndSignUpPage} />
+
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { initialize } from 'workbox-google-analytics';
+
+const config = {
+    apiKey: "AIzaSyA1saXMZRz2RnZ5TGuBKYdjFeWsQz4wyic",
+    authDomain: "crwn-db-9980e.firebaseapp.com",
+    projectId: "crwn-db-9980e",
+    storageBucket: "crwn-db-9980e.appspot.com",
+    messagingSenderId: "410234162589",
+    appId: "1:410234162589:web:2dbe9e5f23fe686633b374",
+    measurementId: "G-0S4WB95JJE"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account'});
+
+  export const signInwithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
